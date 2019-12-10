5b6b1967f6ce020868598f535faa83944fefb662
PapyrusFlexia <45296798+PapyrusFlexia@users.noreply.github.com>
2019-12-10 20:05:12 +0100

    creado buscar elementos asociados a usuario

--- PROYECTO_FINAL_CLIENTE/src/busqueda.js
+++ PROYECTO_FINAL_CLIENTE/src/busqueda.js
@@ -1,3 +1,45 @@
+function mostrarJuegosHTML(juegosMostrar){
+    let divJuegos  = document.getElementById("juegosMostrar");
+    divJuegos.innerHTML = "";
+    if(juegosMostrar.length === 0){
+        divJuegos.innerHTML = "No se ha encontrado ningún juego";
+    }else{
+        juegosMostrar.forEach( juego => juego.mostrarEnHTML(divJuegos));
+    }
+}
+
+function noEspacios(cadena){
+    let arrayPalabras = cadena.split(" ");
+    let nueva = "";
+
+    for (let i = 0; i < arrayPalabras.length; i++) {
+        nueva += arrayPalabras[i];
+    }
+    return nueva;
+}
+
+/**
+ * Funcion que recibe una cadena, le quita los espacios en blanco por delante y por detras y la pone en mayusculas
+ * @param {string} cadena es la cadena a tratar
+ */
+function quitarEspaciosArrays(array){
+    let arrayResultante = null;
+    if(typeof(array) === "string"){
+        let arrayRegex = array.trim().toUpperCase();
+        arrayRegex = arrayRegex.replace(/\s{2,}/g," ");
+        if(arrayRegex !== ""){
+            arrayResultante = arrayRegex;
+        }
+    }
+    return arrayResultante;
+}
+
+function incluirJuegoHTML(juego){
+    let divJuegos  = document.getElementById("juegosMostrar");
+    juego.mostrarEnHTML(divJuegos);
+}
+
+
 function filtrarTituloTipo(){
     let inputTitulo = document.getElementById("titulo");
     let inputTipo = document.getElementById("tipo");
@@ -16,7 +58,7 @@ function filtrarTituloTipo(){
     
 }
 
-function realizarBusqueda(titulo, tipo){
+function realizarBusqueda(titulo, genero){
 	let juegosResultantes = juegos.filter( juego => juego.contieneTitulo(titulo) && juego.contieneTipo(tipo));
 	return juegosResultantes;
 }
@@ -25,12 +67,39 @@ function filtrarGenero(){
     let inputGenero = document.getElementById("genero");
     let genero  = inputGenero.value.trim().toLowerCase();
     let juegosResultantes = similar(genero);
-    mostrarPeliculasHTML(juegosResultantes);
+    mostrarJuegosHTML(juegosResultantes);
 }
 
 function similar(genero){
     let juegosResultantes = juegos.filter( juego => juegos.contieneGenero(juegos));
     return juegosResultantes;
+}
+
+/* funcion para mostrar los juegos comprados del usuario */
+
+function juegosComprados(){
+    let inputUsuario = document.getElementById("usuario");
+    let nombre = inputUsuario.value.trim().toUpperCase();
+    let peliculasFavoritas = [];
+    let esNombreCorrecto = validarNombreUsuario(inputUsuario,erroresFiltrarPF);
+
+    if(esNombreCorrecto){
+        peliculasFavoritas = peliculasUsuario(nombre);
+            mostrarPeliculasHTML(juegosComprados);
+       
+    }
+}
+
+/** devuelve los juegos comprados */
+function juegosUsuario(nombre){
+    let usuario = listaUsuarios.find( usuario => usuario.contieneNombre(nombre));
+    let arrayUsuario = [];
+    if(usuario !== undefined){
+        return usuario.juegosComprados;
+    }else{
+        return arrayUsuario;
+    }
+    
 }
 
 
--- PROYECTO_FINAL_CLIENTE/src/clases.js
+++ PROYECTO_FINAL_CLIENTE/src/clases.js
@@ -38,3 +38,183 @@ class Tienda{
 	}
 
 }
+
+let usuariouno= new Cliente("Pablo","Vega Sanchez");
+let usuariodos = new Cliente("Carlos", "García Mármol");
+let listaUsuarios = [usuariouno,usuariodos];
+
+usuariouno.incluirJuegosComprados(juego1);   /** NOMBRES DE LOS JUEGOS */
+usuariouno.incluirJuegosComprados(juego2);
+usuariodos.incluirJuegosComprados(juego3);
+usuariodos.incluirJuegosComprados(juego4);
+
+
+class Usuario{
+
+	constructor(nombre, apellidos){
+		super(nombre,apellidos);
+		this.juegosComprados = [];
+		this.votos = [];
+	}
+
+	get nombre(){
+		return `${this._nombre}`;
+	}
+
+	set nombre(nombre){
+		this._nombre=nombre;
+	}
+
+	get apellidos(){
+		return `${this._apellidos}`;
+	}
+
+	set apellidos(apellidos){
+		this._apellidos=apellidos;
+	}
+
+	get juegosComprados(){
+		return this._juegosComprados;
+	}
+
+	set juegosComprados(juegos){
+		this._juegosComprados=juegos;
+	}
+
+	get votos(){
+		return this._votos;
+	}
+
+	set votos(votos){
+		this._votos=votos;
+	}
+	
+	incluirJuegosComprados(juego){
+		this._juegosComprados.push(juego);
+	}
+
+	contieneNombre(nombre){
+		let contiene = false;
+		let nombreEncontrado = quitarEspaciosArrays(nombre);
+		if(nombreEncontrado !== null){
+			contiene = this.nombre.toUpperCase().includes(nombreEncontrado);
+		}
+		return contiene;
+	}
+}
+
+class Juego{
+
+	
+	constructor(titulo,creador,genero,editorial){
+		this.titulo = titulo;
+		this.creador = creador;
+		this.genero = genero;
+		this.editorial = editorial;
+		this.votos = [];
+		this.likes = 0;
+		this.dislikes = 0;
+	}
+
+	get titulo(){
+		return this._titulo;
+	}
+
+	set titulo(titulo){
+		this._titulo=titulo;
+	}
+
+	get editorial(){
+		return this._editorial;
+	}
+
+	set editorial(editorial){
+		this._editorial=editorial;
+	}
+
+	get creador(){
+		return this._creador;
+	}
+
+	set creador(creador){
+		this._creador=creador;
+	}
+
+	get genero(){
+		return this._genero;
+	}
+
+	set genero(genero){
+		this._genero=genero;
+	}
+
+	get votos(){
+		return this._votos;
+	}
+
+	set votos(votos){
+		this._votos=votos;
+	}
+
+	incluirJuegosComprados(juego){
+		this._juegosComprados.push(juego);
+	}
+
+	contieneTitulo(titulo){
+		let contiene = false;
+		let tituloEncontrado = quitarEspaciosArrays(titulo);
+		if(tituloEncontrado !== null){
+			contiene = this.titulo.toUpperCase().includes(tituloEncontrado);
+		}
+		return contiene;
+	}
+	
+	contieneTipo(tipo){
+		let contiene = false;
+		let tipoEncontrado = quitarEspaciosArrays(tipo);
+		if(tipoEncontrado != null){
+			contiene = this.tipo.toUpperCase().includes(tipoEncontrado);
+		}
+		return contiene;
+	}
+	
+}
+
+
+mostrarEnHTML(nodoHTML){
+	let bloque = document.createElement("div");
+	bloque.className = "juego";
+	bloque.setAttribute("data-identificador", noEspacios(this.titulo));
+	let titulo = document.createElement("h2");
+	titulo.innerHTML = this.titulo; 
+	titulo.setAttribute("data-identificador","titulo");
+	let genero = document.createElement("h3");
+	genero.innerHTML = this.genero;
+	genero.setAttribute("data-identificador","genero");
+	let creador = document.createElement("p");
+	creador.innerHTML = `${this.creador}`;
+	creador.setAttribute("data-identificador","creador");
+	let editorial = document.createElement("p");
+	editorial.innerHTML = `${this.editorial}`;
+	editorial.setAttribute("data-identificador","editorial");
+	let contadorLikes = document.createElement("i");
+	contadorLikes.setAttribute("data-identificador","contadorLikes");
+	contadorLikes.classList.add("far");
+	contadorLikes.classList.add("fa-thumbs-up");
+	contadorLikes.innerHTML = `${this.likes}`;
+	let contadorDislikes = document.createElement("i");
+	contadorDislikes.setAttribute("data-identificador","contadorDislikes");
+	contadorDislikes.classList.add("far");
+	contadorDislikes.classList.add("fa-thumbs-down");
+	contadorDislikes.innerHTML = `${this.dislikes}`;
+	bloque.append(titulo);
+	bloque.append(genero);
+	bloque.append(creador);
+	bloque.append(editorial);
+	bloque.append(contadorLikes);
+	bloque.append(contadorDislikes);
+	nodoHTML.appendChild(bloque);
+}
+
+
+
